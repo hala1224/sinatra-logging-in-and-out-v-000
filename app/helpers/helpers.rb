89fe8ffbfe63ec034a8cb5bdc8987ec03aa8ac61
Natalie Halawani <hala1224@gmail.com>
2019-03-10 19:41:41 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,10 +3,8 @@ class Helpers
   
   def current_user 
     
-   c_user = User.find_by_id(session[:user_id])
-   c_user.save
-   c_user
-    
+   User.find_by_id(session[:user_id])
+   
   end
   
   def is_logged_in?
